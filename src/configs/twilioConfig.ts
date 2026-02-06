98d079106cfef92f67e2ec8185cadb45722761c3
SIVA PRASAD <ispr54@gmail.com>
2026-02-06 12:20:58 +0530

    fix: add missing from number to twilio config

--- src/configs/twilioConfig.ts
+++ src/configs/twilioConfig.ts
@@ -2,16 +2,17 @@ import twilio from "twilio";
 import dotenv from 'dotenv'
 dotenv.config()
 
-const twilioClient = twilio(process.env.TWILIO_ACCOUNT_SID , process.env.TWILIO_AUTH_TOKEN)
+const twilioClient = twilio(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN)
 
-const sendMessage = async (mobileNumber:number , otp:number)=>{
-    try{
+const sendMessage = async (mobileNumber: number, otp: number) => {
+    try {
         const sendMessage = await twilioClient.messages.create({
-            body:`Your otp for a1care login ${otp}, have a great day`, 
-            to:`+91${mobileNumber}`
+            body: `Your otp for a1care login ${otp}, have a great day`,
+            to: `+91${mobileNumber}`,
+            from: process.env.TWILIO_PHONE_NUMBER
         })
-    }catch(error){
-        console.error('Error in sending the message' , error)
+    } catch (error) {
+        console.error('Error in sending the message', error)
     }
 }
 
